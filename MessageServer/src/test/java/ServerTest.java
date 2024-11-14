0f69d6590f5bc1e9b36e7303fa3d121eccd19997
Александр Трунин <masstadontt@rambler.ru>
2024-11-14 23:14:15 +0300

    Hw

--- MessageServer/src/test/java/ServerTest.java
+++ MessageServer/src/test/java/ServerTest.java
@@ -1,32 +1,112 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.CompletableFuture;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import Classes.NewClient;
+import Classes.Server;
+
 class ServerTest {
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
+	//private static ArrayList<NewClient> clients = new ArrayList<NewClient>();	
+			private static ServerSocket lestener; 
+			
+			private static int port = 9464;
+			private static String host = "localhost";
+			
+			private Server serv = new Server();
 
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
+			@BeforeAll
+			static void setUpBeforeClass() throws Exception {
+				lestener = new ServerSocket(port);
+				System.out.println("listener start");
+			}
 
-	@BeforeEach
-	void setUp() throws Exception {
-	}
+			@AfterAll
+			static void tearDownAfterClass() throws Exception {
+				lestener.close();
+				System.out.println("listener closed");
+			}
 
-	@AfterEach
-	void tearDown() throws Exception {
-	}
+			
+			@BeforeEach
+			void setUp() throws Exception {
+			}
+			
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+			@AfterEach
+			void tearDown() throws Exception {
+			}
+			
+
+			@Test
+			void testRun() {	
+				
+				// new Thread for running method
+				CompletableFuture.runAsync(() -> {
+					serv.run();
+				});
+				
+				// add new clients
+				for(int i = 0 ; i < 2 ; ++i) {				
+					try {
+						Socket server = new Socket(host, serv.port);	
+						
+						ObjectOutputStream out = new ObjectOutputStream(server.getOutputStream());
+						
+						Thread.sleep(500);
+					} 
+					catch (InterruptedException e) {					
+							e.printStackTrace();
+					}
+					catch (IOException e) {				
+						e.printStackTrace();
+					}	
+				}
+				
+				assertNotNull(serv.lestener);		
+				assert(serv.clients.size() > 0);
+			}
+			
+
+			@Test
+			void testListenClient() {
+				fail("Not yet implemented");
+			}
+			
+
+			@Test
+			void testSenddAll() {
+				
+				// подключение новых клиентов к серверу
+				for(int i = 0 ; i < 100 ; ++i) {
+					
+					Socket client = null;			
+					try {
+						Socket server = new Socket(host, port);	
+						
+						client = lestener.accept();	
+						
+						ObjectOutputStream out = new ObjectOutputStream(server.getOutputStream());
+					} 
+					catch (IOException e) {				
+						e.printStackTrace();
+					}
+					
+					serv.clients.add(new NewClient(client));
+				}
+				
+				assert(serv.senddAll("test") == serv.clients.size());
+				
+			}
 
 }
